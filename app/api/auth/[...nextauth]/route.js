9c73a0e7c45616092e958ad843284e9357c67819
bishal <bishals10@gmail.com>
2023-09-18 20:20:21 +0300

    add signIn and session functionality to route, and add next-auth env variables to .env

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -8,12 +10,35 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
 
   async signIn({ profile }) {
     try {
       // esrverless route -> means it is Lambda function that opens up only when it is called. it spins up server functions and make connection to database. We do not need to keep connected to server. -> dynamodb
-    } catch (error) {}
+      await connectToDB();
+
+      // check if a user already exists
+      const userExists = await User.findOne({ email: profile.email });
+
+      // if no user, create a new user and save to database
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
