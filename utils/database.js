475fe1281e82f55c732bda69226d3cee5c6dfa93
bishal <bishals10@gmail.com>
2023-09-18 19:39:16 +0300

    configure database.js in utils to connect to mongodb using mongoose

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+// To hook it to database
+
+import mongoose from "mongoose";
+
+let isConnected = false; // to track connection status
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+  // try to establish connection
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
