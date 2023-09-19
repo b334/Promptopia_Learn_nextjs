5f80f71c092ea595a0ed20bf1253761fc3df3d9f
bishal <bishals10@gmail.com>
2023-09-19 20:41:41 +0300

    connect mongodb/atlas database

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia_Learning-project ",
   description: "Discover and share AI Prompts",
@@ -8,10 +10,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">{children}</main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
